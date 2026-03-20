a62ae7eff5a94735c8a49cd102e42bbab508771e
MamaiTheCoder <jmakoks@gmail.com>
2026-03-20 17:27:54 +0300

    Create CRUD operation

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -12,9 +12,81 @@ exports.newProduct = async (req, res) => {
   }
 };
 
-exports.getProducts = (req, res, next) => {
+// Get all products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: "This route will show all products in the database",
+    count: products.length,
+    products,
   });
+};
+
+// Get a single product details
+exports.getProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Update Product
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Delete product
+exports.deleteProduct = async (req, res) => {
+  try {
+    const productToDelete = await Product.findByIdAndDelete(req.params.id);
+
+    if (!productToDelete) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,9 +5,16 @@ const router = express.Router();
 const {
   getProducts,
   newProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/product.controller");
 
 router.route("/products").get(getProducts);
 
 router.route("/products").post(newProduct);
+router.route("/products/:id").get(getProduct);
+router.route("/admin/products/:id").put(updateProduct);
+router.route("/admin/products/:id").delete(deleteProduct);
+
 module.exports = router;
